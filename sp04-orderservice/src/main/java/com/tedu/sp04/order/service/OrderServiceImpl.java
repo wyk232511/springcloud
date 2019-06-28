3dbe9b007ad28733ee36d1d713ab1edbbcabdfe5
wyk232511 <1250249290@qq.com>
2019-06-28 10:22:32 +0800

    test

--- sp04-orderservice/src/main/java/com/tedu/sp04/order/service/OrderServiceImpl.java
+++ sp04-orderservice/src/main/java/com/tedu/sp04/order/service/OrderServiceImpl.java
@@ -1,5 +1,11 @@
 package com.tedu.sp04.order.service;
 
+import com.tedu.sp01.pojo.Item;
+import com.tedu.sp01.pojo.User;
+import com.tedu.sp04.order.feignclient.ItemFeignService;
+import com.tedu.sp04.order.feignclient.UserFeignService;
+import com.tedu.web.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tedu.sp01.pojo.Order;
@@ -7,23 +13,35 @@ import com.tedu.sp01.service.OrderService;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private ItemFeignService itemFeignService;
+    @Autowired
+    private UserFeignService userFeignService;
 
     @Override
     public Order getOrder(String orderId) {
         //TODO: 调用user-service获取用户信息
+        JsonResult<User> user = userFeignService.getUser(7);
         //TODO: 调用item-service获取商品信息
+        JsonResult<List<Item>> items = itemFeignService.getItems(orderId);
         Order order = new Order();
         order.setId(orderId);
+        order.setUser(user.getData());
+        order.setItems(items.getData());
         return order;
     }
 
     @Override
     public void addOrder(Order order) {
         //TODO: 调用item-service减少商品库存
+        itemFeignService.decreaseNumber(order.getItems());
         //TODO: 调用user-service增加用户积分
+        userFeignService.addScore(7,100);
         log.info("保存订单："+order);
     }
 
